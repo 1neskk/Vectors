593ada60005dee7053fc348ba0cba82ff133cae3
Gabriel Almeida <113075816+1neskk@users.noreply.github.com>
2023-04-16 13:40:12 -0300

    Update vec2.hpp

--- vec2.hpp
+++ vec2.hpp
@@ -1,122 +1,268 @@
-#ifndef __VEC2_H__
-#define __VEC2_H__
+#ifndef _VEC2_HPP
+#define _VEC2_HPP
 
 #include <cmath>
+#include <iostream>
+#include <cassert>
 
+// Warning: anonymous unions and structs are not guaranteed to work in every
+//          compiler/C++ version
 
 template <class T>
-class vec2 {
-public:
-	T x, y;
-	
-	vec2() :x(0), y(0) {}
-	vec2(T x, T y) : x(x), y(y) {}
-	vec2(const vec2& v) : x(v.x), y(v.y) {}
-	
-	vec2& operator=(const vec2& v) {
-		x = v.x;
-		y = v.y;
-		return *this;
-	}
-	
-	vec2 operator+(vec2& v) {
-		return vec2(x + v.x, y + v.y);
-	}
-	vec2 operator-(vec2& v) {
-		return vec2(x - v.x, y - v.y);
-	}
-	
-	vec2& operator+=(vec2& v) {
-		x += v.x;
-		y += v.y;
-		return *this;
-	}
-	vec2& operator-=(vec2& v) {
-		x -= v.x;
-		y -= v.y;
-		return *this;
-	}
-	
-	vec2 operator+(double s) {
-		return vec2(x + s, y + s);
-	}
-	vec2 operator-(double s) {
-		return vec2(x - s, y - s);
-	}
-	vec2 operator*(double s) {
-		return vec2(x * s, y * s);
-	}
-	vec2 operator/(double s) {
-		return vec2(x / s, y / s);
-	}
-	
-	
-	vec2& operator+=(double s) {
-		x += s;
-		y += s;
-		return *this;
-	}
-	vec2& operator-=(double s) {
-		x -= s;
-		y -= s;
-		return *this;
-	}
-	vec2& operator*=(double s) {
-		x *= s;
-		y *= s;
-		return *this;
-	}
-	vec2& operator/=(double s) {
-		x /= s;
-		y /= s;
-		return *this;
-	}
-	
-	void set(T x, T y) {
-		this->x = x;
-		this->y = y;
-	}
-	
-	void rotate(double deg) {
-		double theta = deg / 180.0 * M_PI;
-		double c = cos(theta);
-		double s = sin(theta);
-		double tx = x * c - y * s;
-		double ty = x * s + y * c;
-		x = tx;
-		y = ty;
-	}
-	
-	vec2& normalize() {
-		if (length() == 0) return *this;
-		*this *= (1.0 / length());
-		return *this;
-	}
-	
-	float length() const {
-		return std::sqrt(x * x + y * y);
-	}
-	void truncate(double length) {
-		double angle = atan2f(y, x);
-		x = length * cos(angle);
-		y = length * sin(angle);
-	}
-	
-	vec2 ortho() const {
-		return vec2(y, -x);
-	}
-	
-	static float dot(vec2 v1, vec2 v2) {
-		return v1.x * v2.x + v1.y * v2.y;
-	}
-	static float cross(vec2 v1, vec2 v2) {
-		return (v1.x * v2.y) - (v1.y * v2.x);
-	}
-	
-};
+class Vec2
+	{
+	public:
+		union
+			{
+			struct { T x,y; };
+			struct { T s,t; };
+			struct { T u,v; };
+			struct { T V[2]; };
+			};
 
-typedef vec2<float> vec2f;
-typedef vec2<double> vec2d;
+		Vec2<T>() { x=y=0; }
 
+		Vec2<T>(T cx, T cy) { x=cx; y=cy; }
+
+		Vec2<T>(T cx) { x=y=cx; }
+
+		Vec2<T>(const Vec2<T> &v) { x=v.x; y=v.y; }
+
+		Vec2<T>(T v[2]) { x=v[0]; y=v[1]; }
+
+		inline T dot() const
+			{
+			T r=x*x+y*y;
+			return(r);
+			}
+
+		inline T dot(const Vec2<T> &q) const
+			{
+			T r=x*q.x+y*q.y;
+			return(r);
+			}
+
+		inline T length() const
+			{
+			T r=sqrt(x*x+y*y);
+			return(r);
+			}
+
+		inline Vec2<T> const normalized() const
+			{
+			T mag=sqrt(x*x+y*y);
+			if (mag!=0.0)
+				{
+				Vec2<T> r;
+				r.x=x/mag; r.y=y/mag;
+				return(r);
+				}
+			return(Vec2<T>(0.0,0.0));
+			}
+
+		inline T normalize()
+			{
+			T mag=sqrt(x*x+y*y);
+			if (mag!=0.0) { x/=mag; y/=mag; }
+			else { x=y=0.0; }
+			return(mag);
+			}
+
+		inline T operator[] (int i) const
+			{
+			assert((i>=0)&&(i<2));
+			return(V[i]);
+			}
+
+		inline T& operator[] (int i)
+			{
+			assert((i>=0)&&(i<2));
+			return(V[i]);
+			}
+
+	friend Vec2<T> operator+ (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		Vec2<T> c; c.x=L.x+R.x; c.y=L.y+R.y; return(c);
+		}
+
+	friend Vec2<T> operator+ (const Vec2<T> &L, const T &R)
+		{
+		Vec2<T> c; c.x=L.x+R; c.y=L.y+R; return(c);
+		}
+
+	friend Vec2<T> operator+ (const T &L, const Vec2<T> &R)
+		{
+		Vec2<T> c; c.x=L+R.x; c.y=L+R.y; return(c);
+		}
+
+	friend Vec2<T> operator- (const Vec2<T> &R)
+		{
+		Vec2<T> c; c.x=-R.x; c.y=-R.y; return(c);
+		}
+
+	friend Vec2<T> operator- (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		Vec2<T> c; c.x=L.x-R.x; c.y=L.y-R.y; return(c);
+		}
+
+	friend Vec2<T> operator- (const Vec2<T> &L, const T &R)
+		{
+		Vec2<T> c; c.x=L.x-R; c.y=L.y-R; return(c);
+		}
+
+	friend Vec2<T> operator- (const T &L, const Vec2<T> &R)
+		{
+		Vec2<T> c; c.x=L-R.x; c.y=L-R.y; return(c);
+		}
+
+	friend Vec2<T> operator* (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		Vec2<T> c;
+		c.x=R.x*L.x; c.y=R.y*L.y;
+		return(c);
+		}
+
+	friend Vec2<T> operator* (const T &L, const Vec2<T> &R)
+		{
+		Vec2<T> c;
+		c.x=R.x*L; c.y=R.y*L;
+		return(c);
+		}
+
+	friend Vec2<T> operator* (const Vec2<T> &L, const T &R)
+		{
+		return(R*L);
+		}
+
+	friend Vec2<T> operator/ (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		Vec2<T> c;
+		c.x=L.x/R.x; c.y=L.y/R.y;
+		return(c);
+		}
+
+	friend Vec2<T> operator/ (const Vec2<T> &L, const T &R)
+		{
+		Vec2<T> c;
+		c.x=L.x/R; c.y=L.y/R;
+		return(c);
+		}
+
+	friend Vec2<T>& operator+= (Vec2<T> &L, const Vec2<T> &R)
+		{
+		L.x+=R.x; L.y+=R.y;
+		return(L);
+		}
+
+	friend Vec2<T>& operator+= (Vec2<T> &L, const T &R)
+		{
+		L.x+=R; L.y+=R;
+		return(L);
+		}
+
+	friend Vec2<T>& operator-= (Vec2<T> &L, const Vec2<T> &R)
+		{
+		L.x-=R.x; L.y-=R.y;
+		return(L);
+		}
+
+	friend Vec2<T>& operator-= (Vec2<T> &L, const T &R)
+		{
+		L.x-=R; L.y-=R;
+		return(L);
+		}
+
+	friend Vec2<T>& operator*= (Vec2<T> &L, const Vec2<T> &R)
+		{
+		L.x*=R.x; L.y*=R.y;
+		return(L);
+		}
+
+	friend Vec2<T>& operator*= (Vec2<T> &L, const T &R)
+		{
+		L.x*=R; L.y*=R;
+		return(L);
+		}
+
+	friend Vec2<T>& operator/= (Vec2<T> &L, const Vec2<T> &R)
+		{
+		L.x/=R.x; L.y/=R.y;
+		return(L);
+		}
+
+	friend Vec2<T>& operator/= (Vec2<T> &L, const T &R)
+		{
+		L.x/=R; L.y/=R;
+		return(L);
+		}
+
+	friend Vec2<T>& operator++ (Vec2<T> &R)
+		{
+		++R.x; ++R.y;
+		return(R);
+		}
+
+	friend Vec2<T> operator++ (Vec2<T> &L, int)
+		{
+		Vec2<T> c=L;
+		L.x++; L.y++;
+		return(c);
+		}
+
+	friend Vec2<T>& operator-- (Vec2<T> &R)
+		{
+		--R.x; --R.y;
+		return(R);
+		}
+
+	friend Vec2<T> operator-- (Vec2<T> &L, int)
+		{
+		Vec2<T> c=L;
+		L.x--; L.y--;
+		return(c);
+		}
+
+#ifdef EPSILONCOMP
+
+#define eq(a,b) (fabs(a-b)<EPSILONCOMP)
+#define ne(a,b) (fabs(a-b)>=EPSILONCOMP)
+
+	friend bool operator== (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		return((eq(L.x,R.x))&&(eq(L.y,R.y)));
+		}
+
+	friend bool operator!= (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		return((ne(L.x,R.x))||(ne(L.y,R.y)));
+		}
+
+#undef eq
+#undef ne
+
+#else
+
+	friend bool operator== (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		return((L.x==R.x)&&(L.y==R.y));
+		}
+
+	friend bool operator!= (const Vec2<T> &L, const Vec2<T> &R)
+		{
+		return((L.x!=R.x)||(L.y!=R.y));
+		}
+
+#endif
+
+	friend std::ostream& operator<< (std::ostream &os, const Vec2<T> &R)
+		{
+		os << "(" << R.x << "," << R.y << ")";
+		return(os);
+		}
+	};
+
+typedef Vec2<float> vec2;
+typedef Vec2<int> ivec2;
+typedef Vec2<bool> bvec2;
 
 #endif
