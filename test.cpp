a7c98c8f7eae0548cdda57929f6bd0068e1bf20d
1nullptr <129955496+1nullptr@users.noreply.github.com>
2023-04-24 18:52:33 -0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,18 +2,11 @@
 
 int main()
 {
-    Vec2 v1(2.0f, 1.0f);
-    Vec2 v2(4.0f, 3.0f);
-    std::cout << v1 + v2 << std::endl;
-    std::cout << v2 << std::endl;
-    Vec2 v3 = v1 + v2;
-    Vec2 v4 = v1 - v2;
-    Vec2 v5 = v1 * 2;
-    Vec2 v6 = v1 / 2;
-    Vec2 v7 = -v1;
-    float dot = v1.dot(v2);
-    float length = v1.length();
-    Vec2 v8 = v1.normalize();
+    Vec2 v1(5.581f, -2.136f);
+    Vec3 v2(1.996f, 3.108f, -4.554);
+    std::cout << v1.normalize() << std::endl;
+    std::cout << v2.normalize() << std::endl;
+    
     return 0;
 }
 //g++ test.cpp -o test
